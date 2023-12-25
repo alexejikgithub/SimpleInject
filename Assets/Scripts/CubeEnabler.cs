52a64d34adbbfa1aea30da77522a680eb130ef4e
alexejikgithub <alexeysmirnov1992@gmail.com>
2023-12-26 02:09:00 +0400

    Minor naming changes

--- Assets/Scripts/CubeEnabler.cs
+++ Assets/Scripts/CubeEnabler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level : MonoBehaviour
+public class CubeEnabler : MonoBehaviour
 {
     [Inject] private Cube _cube;
     void Awake()
